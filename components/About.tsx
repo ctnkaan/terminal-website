e2893e2efb1d0ece257c9cf675fa189d173e1d9d
ctnkaan <kaan.taskingenc@gmail.com>
2022-03-06 04:47:39 +0300

    updated about me section

--- components/About.tsx
+++ components/About.tsx
@@ -2,28 +2,17 @@ import React from 'react'
 import developerGif from "../assets/Developer.gif"
 import Image from 'next/image'
 import AboutStyles from "../styles/About.module.scss"
+import {Container, Row, Col} from "react-bootstrap"
 
 function About() {
   return (
-    <div>
-      {/* About me*/},
-      <div className={AboutStyles.about}>
-          <h1>About Me</h1>
-
-          <div className={AboutStyles.row}>
-            <Image src={developerGif} alt="Developer Gif" />
-            <p>
-              I am a full stack developer with a passion for learning and
-              problem solving. I have a background in business and finance, and
-              have worked in the financial services industry for over 10 years.
-              I am currently working at a startup in the financial services
-              industry, where I am building a web application to help small
-              businesses manage their finances.
-            </p>
-          </div>
-
-        </div>
-    </div>
+      <Container className={AboutStyles.about}>
+        <h1>About Me</h1>
+        <Row>
+          <Col lg> <Image src={developerGif} alt="Developer Gif" /></Col>
+          <Col lg className={AboutStyles.text}>I am an aspiring software developer/student with 7 months of professional experience. I love developing web applications both in front-end and back-end. I mainly mentor other fellow engineering students who want to start building up a career. I am a leader at many student communities such as Microsoft Learn Student Ambassadors, Postman Student Leaders, IEEE Student Branches Computer Society and Voice of Code.</Col>
+        </Row>
+      </Container>
   )
 }
 
--- components/NavbarComponent.tsx
+++ components/NavbarComponent.tsx
@@ -11,20 +11,21 @@ function NavbarComponent() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      <Nav.Link href="#about">about</Nav.Link>
+      <Nav.Link href="#skills">Skills</Nav.Link>
+      <NavDropdown title="Experience" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#experience">Professional Experience</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item href="#volunteer">Volunteer Experience</NavDropdown.Item>
       </NavDropdown>
+      <Nav.Link href="#certifications">Certifications</Nav.Link>
+      <Nav.Link href="#workshops">Workshops</Nav.Link>
+      <Nav.Link href="#references">References</Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
+      <Nav.Link href="#contact">Contact</Nav.Link>
+      <Nav.Link href="linkedIn">
+        LinkedIn
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
