28ad6512698af799caab7a6d03c2f0ad3713dc6e
ctnkaan <kaan.taskingenc@gmail.com>
2022-03-17 12:43:21 +0300

    added animations to projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 import CardTemplate from '../templates/CardTemplate'
 import CardTempStyles from '../styles/CardTemplate.module.scss'
 
+
 function Projects() {
   return (
     <Container id="projects" className={CardTempStyles.ContainerStyles}>
@@ -15,6 +16,7 @@ function Projects() {
             image_url="https://miro.medium.com/max/724/1*LvA59wJi3O9jTMQQsw_cRA.png"
             text="This is my portfolio website. It is built with React and Next.js."
             button_link="https://github.com/ctnkaan/portfolio"
+            animation='animate__flip'
           />
         </Col>
 
@@ -24,6 +26,7 @@ function Projects() {
             image_url="https://raw.githubusercontent.com/ctnkaan/Postman-Student-Helper/main/assets/icon.png"
             text="A cyber security Discord bot built for protecting Postman Discord server. It has blocked more than 500 attacks."
             button_link="https://github.com/ctnkaan/Postman-Student-Helper"
+            animation='animate__flip'
           />
         </Col>
 
@@ -33,6 +36,7 @@ function Projects() {
             image_url="https://images.emojiterra.com/google/android-11/512px/1f916.png"
             text="A CLI that can make you a discord bot. It has options for JavaScript and TypeScript."
             button_link="https://github.com/ctnkaan/make-discord-bot"
+            animation='animate__flip'
           />
         </Col>
       </Row>
@@ -46,6 +50,7 @@ function Projects() {
             image_url="https://raw.githubusercontent.com/ctnkaan/Noodle/main/images/noodle.png"
             text="A multipurpouse Discord bot that can play music, play games, and more. I learned a lot developing this bot specially in DevOps."
             button_link="https://github.com/ctnkaan/Noodle"
+            animation='animate__flip'
           />
         </Col>
 
@@ -55,6 +60,7 @@ function Projects() {
             image_url="https://cdn-icons-png.flaticon.com/512/1555/1555512.png"
             text="A weather app made with Node.js and OpenWeatherMap API."
             button_link="https://github.com/ctnkaan/WeatherApp"
+            animation='animate__flip'
           />
         </Col>
 
@@ -64,6 +70,7 @@ function Projects() {
             image_url="https://thumbs.dreamstime.com/b/business-to-do-list-flat-icon-modern-style-task-list-business-to-do-list-flat-icon-modern-style-any-purposes-perfect-web-138650221.jpg"
             text="A todo list made with React and MongoDB. It has specific endpoints for users."
             button_link="https://github.com/ctnkaan/ToDo"
+            animation='animate__flip'
           />
         </Col>
 
--- templates/CardTemplate.tsx
+++ templates/CardTemplate.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import {Card, Button} from "react-bootstrap";
 import CardTempStyles from "../styles/CardTemplate.module.scss";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 interface propsType {
     title: string,
     image_url: string,
     text: string,
     button_link: string,
+    animation: string
 }
 
 
 function CardTemplate(props : propsType) {
   return (
+    <AnimationOnScroll animateIn={props.animation} animateOnce={true}>
     <div>
         <Card className={CardTempStyles.CardStyles}>
         <Card.Img height={250} variant="top" src={props.image_url} />
@@ -24,6 +27,7 @@ function CardTemplate(props : propsType) {
           </Card.Body>
         </Card>
     </div>
+    </AnimationOnScroll>
   )
 }
 
