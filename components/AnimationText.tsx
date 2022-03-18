d66bfecadf1b8a9bf0bd756340dacf37f766889e
ctnkaan <kaan.taskingenc@gmail.com>
2022-03-18 14:43:24 +0300

    added profile picture

--- components/AnimationText.tsx
+++ components/AnimationText.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import AnimationTextStyles from "../styles/AnimationText.module.scss"
+import Image from "next/image"
+import ProfileImage from ".//../assets/cetinkaantaskingenc.jpg"
 
 function AnimationText() {
   return (
     <div>
+
+      <div className={AnimationTextStyles.container}>
+        <Image 
+          src={ProfileImage}
+          alt="cetinkaantaskingenc"
+          width={200}
+          height={200}
+          className={AnimationTextStyles.profileImage}
+        />
+      </div>
+
+
         {/*eslint-disable-next-line react/no-unescaped-entities*/}
         <h1 className={AnimationTextStyles.header}>Hi👋<br/><br/> I'm Çetin Kaan Taşkıngenç <br/><br/> A Fullstack Developer</h1>
 
